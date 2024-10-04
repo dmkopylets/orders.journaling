abbbb95d02ddc082249264de5dce2197bdfb3e34
dmKopylets <dm.kopylets@gmail.com>
2024-10-04 17:09:40 +0300

    docker apache configuring in process

--- resources/views/layouts/bootstrapjs.blade.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="{{asset('js/jquery.slim.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
-<script type="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
